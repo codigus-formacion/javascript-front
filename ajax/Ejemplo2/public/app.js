0214ebe9c09c44884980d1f5a07f07ef1d9c3a97
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2024-11-26 11:28:24 +0100

    Update "Ejemplo2"

--- ajax/Ejemplo2/public/app.js
+++ ajax/Ejemplo2/public/app.js
@@ -1,15 +1,13 @@
-async function checkInfo(){
+async function checkInfo() {
+  const info = "someInfo";
 
-    const info = 'someInfo';
+  const response = await fetch(`/checkInfo?info=${info}`);
 
-    const response = await fetch(`/checkInfo?info=${info}`);
+  const infoCheck = await response.json();
 
-    const infoCheck = await response.json();
-  
-    const content = document.getElementById("content");
+  const content = document.getElementById("content");
 
-    content.innerHTML = `
+  content.innerHTML = `
         <p>Valid: ${infoCheck.valid}</p>
         <p>Message: ${infoCheck.message}</p>`;
-
 }
--- ajax/Ejemplo2/src/router.js
+++ ajax/Ejemplo2/src/router.js
@@ -1,24 +1,22 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
-    res.render('index', {
-        name: "World"
-    });
+router.get("/", (req, res) => {
+  res.render("index", {
+    name: "World",
+  });
 });
 
-router.get('/checkInfo', (req, res) => {
+router.get("/checkInfo", (req, res) => {
+  let info = req.query.info;
 
-    let info = req.query.info;
+  let response = {
+    valid: false,
+    message: `Info '${info}' not valid`,
+  };
 
-    let response = {
-        valid: false,
-        message: `Info '${info}' not valid`
-    }
-
-    res.json(response);
+  res.json(response);
 });
 
 export default router;
