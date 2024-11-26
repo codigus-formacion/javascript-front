b41248443e2641d129e36be06b357e4e8bbfbd3e
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2024-11-26 12:02:23 +0100

    Add "Ejercicio2Enunciado" and "Ejercicio2Solucion"

---
+++ ajax/Ejercicio2Enunciado/src/router.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    name: "World",
+  });
+});
+
+export default router;
